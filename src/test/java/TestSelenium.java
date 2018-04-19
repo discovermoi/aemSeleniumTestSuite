cb39ff34d93027ccf8342051ea25c48d4b03d7c4
kuma0163 <muthu.kumar@flinders.edu.au>
2018-04-19 10:54:49 +0930

    Comments Beautification on testSelenium.java

--- src/test/java/TestSelenium.java
+++ src/test/java/TestSelenium.java
@@ -1,20 +1,15 @@
 /* Started on 18th of April 2018 === >> Tryst with set up activity.
 This Project aims at:
-- building up test cases for aem applications run against aem instances
-- work against aem instances irrespective of environments
-- attempts to be not so specific - selectors not based on IDs
-- attempts integrating Cucumber BDD
-- attempts integrating TestNG
-- attempts integrating BOBCAT which is more friendly for testing aem authoring activities thru SiteAdmin APIs
-    - it helps in creating pages for authoring and destroys it after testing.
-
-*
-* */
-
-
+= building up test cases for aem applications run against aem instances
+= work against aem instances irrespective of environments
+= attempts to be not so specific - selectors not based on IDs
+= attempts integrating Cucumber BDD
+= attempts integrating TestNG
+= attempts integrating BOBCAT which is more friendly for testing aem authoring activities thru SiteAdmin APIs
+    = it helps in creating pages for authoring and destroys it after testing.
+*/
 import org.openqa.selenium.By;
 import org.openqa.selenium.firefox.FirefoxDriver;
-
 public class TestSelenium {
     public static void main(String[] args){
         FirefoxDriver driver=new FirefoxDriver();
